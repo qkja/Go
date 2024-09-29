5829d34a857c5f7516406bfbc1bf89e1511b22ae
qkja <qkja1205@163.com>
2024-09-29 22:50:20 +0800

    protobuf

---
+++ GRPC/studyProtoBuf/hello_server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"maps"
+	"net"
+	service "studyProtoBuf/hello_server/proto"
+)
+
+type server struct {
+	service.UnimplementedSayHelloServer
+}
+
+func (server) SayHello(ctx context.Context, req *service.HelloRequest) (*service.HelloResponse, error) {
+	return &service.HelloResponse{
+		ResponseMsg: "hello" + req.RequestName,
+	}, nil
+}
+
+func main() {
+	// 1. 开启端口
+	listen, err := net.Listen("tcp", ":8090")
+	if err != nil {
+		fmt.Println("创建端口失败 %v", err)
+		return
+	}
+	// 2. 创建服务
+	grpcServer := grpc.NewServer()
+	// 3.将我们的服务注册到grpc
+	service.RegisterSayHelloServer(grpcServer, &server{})
+
+	// 4. 启动服务
+	err = grpcServer.Serve(listen)
+	if err != nil {
+		fmt.Println("监听失败")
+	}
+	type Programmer struct {
+		Name string
+		City string
+	}
+
+	m1 := map[string]Programmer{
+		"programmer-01": {Name: "陈明勇", City: "深圳"},
+		"programmer-02": {Name: "张三", City: "广州"},
+	}
+	var m2 = maps.Clone(m1)
+	fmt.Printf("m1: %v\n", m1)
+	fmt.Printf("m2: %v\n", m2)
+}
